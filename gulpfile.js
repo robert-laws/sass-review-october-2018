3259281ec5146cb032a6572e21173f9b3a52fb99
Robert Laws <roblaws@me.com>
2018-10-23 11:57:30 +0300

    restructure files & update gulpfile - add browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,32 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var del = require('del');
+var browserSync = require("browser-sync").create();
 
-gulp.task('sass', function() {
-  return gulp.src('scss/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('css'));
-});
-
-gulp.task('del', function() {
-  return del(['css']);
+gulp.task('html', function() {
+  return gulp.src('src/**/*.html')
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.stream())
 })
 
-gulp.task('watch', function() {
-  gulp.watch('scss/*.scss', ['del','sass'])
+gulp.task('sass', function () {
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream())
 });
 
-gulp.task('default', ['del', 'sass', 'watch'])
+gulp.task('del', function () {
+  return del(['dist']);
+})
+
+gulp.task('watch', function () {
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+  gulp.watch(['src/scss/**/*.scss', 'src/*.html'], ['html', 'sass'])
+});
+
+gulp.task('default', ['html', 'sass', 'watch'])
